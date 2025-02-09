35a9657c6db240013f667f2d5861435205912bbc
Bruno Bortolucci <br.bortolucci@gmail.com>
2025-02-09 12:19:55 -0300

    feat: criando modelo base para reserva de sala

---
+++ src/domain/models/reservation.py
@@ -0,0 +1,22 @@
+from uuid import UUID
+from datetime import datetime
+from pydantic import BaseModel, model_validator
+
+
+class ReservationCreate(BaseModel):
+    room_id: UUID
+    user_name: str
+    start_time: datetime
+    end_time: datetime
+
+    @model_validator(mode="after")
+    def validate_times(self) -> "ReservationCreate":
+        if self.start_time < datetime.now():
+            raise ValueError("Data de início não pode ser menor que a data atual")
+        if self.end_time <= self.start_time:
+            raise ValueError("Data de fim deve ser maior que a data de início")
+        return self
+
+
+class ReservationResponse(ReservationCreate):
+    id: UUID
